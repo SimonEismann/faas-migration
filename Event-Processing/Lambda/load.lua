502d090840f47aa799d3fa918e95a6b61d57a8a9
SimonEismann <31125121+SimonEismann@users.noreply.github.com>
2020-12-22 18:35:51 +0100

    Update load.lua

--- Event-Processing/Lambda/load.lua
+++ Event-Processing/Lambda/load.lua
@@ -4,9 +4,13 @@ end
 
 function onCall(callnum)
 	if callnum == 1 then
-		return "[POST]{\"input\": \"{\\\"userId\\\": \\\"user_a\\\", \\\"s3Bucket\\\":\\\"BUCKETPLACEHOLDER\\\", \\\"s3Key\\\": \\\"1_happy_face.jpg\\\"}\", \"stateMachineArn\": \"STATEMACHINEARNPLACEHOLDER\"}URLPLACEHOLDER"
-	elseif callnum == 2 then
-		return "[POST]{\"input\": \"{\\\"userId\\\": \\\"user_a\\\", \\\"s3Bucket\\\":\\\"BUCKETPLACEHOLDER\\\", \\\"s3Key\\\": \\\"4_no_face.jpg\\\"}\", \"stateMachineArn\": \"STATEMACHINEARNPLACEHOLDER\"}URLPLACEHOLDER"
+		return "[POST]{\"type\": \"forecast\", \"source\": \"opweeyozkbvoddlfqnzqjwbhxdnvot\", \"timestamp\": 1566246, \"forecast\": 123, \"forecast_for\": \"hello world\" \"place\": \"myplace\"}URL1PLACEHOLDER"
+	elseif callnum == 2 then	
+		return "[POST]{\"type\": \"temperature\", \"source\": \"opweeyozkbvoddlfqnzqjwbhxdnvot\", \"timestamp\": 1566246, \"value\": 162}URL1PLACEHOLDER"
+	elseif callnum == 3 then	
+		return "[POST]{\"type\": \"state_change\", \"source\": \"opweeyozkbvoddlfqnzqjwbhxdnvot\", \"timestamp\": 1566246, \"message\": \"mymessage\"}URL1PLACEHOLDER"
+	elseif callnum == 4 then	
+		return "[GET]URL2PLACEHOLDER"
 	else
 		return nil;
 	end

6e2b4aec3e46db91bb7d1cbc366cb78233da923c
SimonEismann <31125121+SimonEismann@users.noreply.github.com>
2020-12-22 18:38:48 +0100

    Update run.sh

--- Event-Processing/Lambda/run.sh
+++ Event-Processing/Lambda/run.sh
@@ -8,15 +8,29 @@ git merge master --no-edit
 git add *
 git commit -m "automerge"
 
-# Setup IAM role
+# Deploy
 serverless deploy | tee output.txt
+
+# Collect output
 export STARTLINE=$(grep -n "endpoint" output.txt | cut -d: -f1)
 export POST=$(sed -n '27p' < output.txt | cut -b 10-)
 export GET=$(sed -n '29p' < output.txt | cut -b 9-)
-echo $POST
-echo $GET
-sleep 3000
+
+# Configure load script
+sed -i "s@URL1PLACEHOLDER@$POST@g" load.lua
+sed -i "s@URL2PLACEHOLDER@$GET@g" load.lua
+
+# Run Load
+java -jar httploadgenerator.jar loadgenerator > loadlogs.txt 2>&1 &
+chmod 777 generateConstantLoad.sh
+./generateConstantLoad.sh $EXP_LOAD $EXP_DURATION
+sleep 10
+java -jar httploadgenerator.jar director --ip localhost --load load.csv -o results.csv --lua load.lua --randomize-users -t $EXP_THREATS
+
+# TODO
+sleep 300000
 
 # Shutdown
 serverless remove
 git stash --include-untracked
+

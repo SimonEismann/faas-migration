ea54c7b458211cbc257a9abf42bf09dcf9f8d884
SimonEismann <31125121+SimonEismann@users.noreply.github.com>
2020-12-22 13:01:51 +0100

    Update run.sh

--- Event-Processing/Lambda/run.sh
+++ Event-Processing/Lambda/run.sh
@@ -9,24 +9,8 @@ git add *
 git commit -m "automerge"
 
 # Setup IAM role
-#aws iam create-role --role-name EventProcessing
-#aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonRDSFullAccess --role-name EventProcessing
-#aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --role-name EventProcessing
-#aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonSQSFullAccess --role-name EventProcessing
-#aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/IAMFullAccess --role-name EventProcessing
-#aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonRDSDataFullAccess --role-name EventProcessing
-#aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess --role-name EventProcessing
-#aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AWSStepFunctionsFullAccess --role-name EventProcessing
 serverless deploy --verbose
 
 # Shutdown
 serverless remove --verbose
-#aws iam delete-role --role-name EventProcessing
-#aws iam detach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonRDSFullAccess --role-name EventProcessing
-#aws iam detach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --role-name EventProcessing
-#aws iam detach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonSQSFullAccess --role-name EventProcessing
-#aws iam detach-role-policy --policy-arn arn:aws:iam::aws:policy/IAMFullAccess --role-name EventProcessing
-#aws iam detach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonRDSDataFullAccess --role-name EventProcessing
-#aws iam detach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess --role-name EventProcessing
-#aws iam detach-role-policy --policy-arn arn:aws:iam::aws:policy/AWSStepFunctionsFullAccess --role-name EventProcessing
 git stash --include-untracked
